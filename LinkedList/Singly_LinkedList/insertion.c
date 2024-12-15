d1eda14f00f7d39e56de3ae9804798be06d29f40
ParthibBiswas10 <parthiblm10@gmail.com>
2024-12-15 15:15:36 +0530

    new

--- LinkedList/Singly_LinkedList/insertion.c
+++ LinkedList/Singly_LinkedList/insertion.c
@@ -13,48 +13,72 @@ void traversal(struct node *ptr)
         }
         printf("NULL");
     }
-/*struct node *insertend(struct node*head,int data){
-    struct node *ptr=(struct node*)malloc(sizeof(struct node));
-    ptr->data=data;
-    ptr->next=head;
-    return ptr;
-}*/
+
 struct node* createnode(int value){
     struct node* newnode=(struct node*)malloc(sizeof(struct node));
     newnode->data=value;
     newnode->next=NULL;
     return newnode;
 }
+struct node* insertbegin(struct node** head,int data){
+    struct node* newnode=createnode(data);
+    newnode->next=*head;
+    *head=newnode;
+    return *head;
+
+}
+struct node* insertend(struct node** head,int data,struct node** temp){
+    struct node* newnode=createnode(data);
+        if(*head==NULL){
+            *head=newnode;
+            *temp=*head;
+        }
+        else{
+            (*temp)->next=newnode;
+            *temp=newnode;
+        }
+        
+
+}
 int main(){
     
     struct node*head=NULL;
     struct node*temp=NULL;
     
-    int n,data;
-    printf("Kota nodes chai?? \n");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
+    int n,data,choice,a;
+    printf("Nodes chai na Nudes??\n");
+    printf("enter 1 for Nodes, enter 2 for Nudes: ");
+    scanf("%d",&a);
+    switch(a){
+        case 1: 
+        //cleanup(&head);
+         printf("Kota nodes chai?? \n");
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++){
         printf("%d node er value: ",i);
         scanf("%d",&data);
-        struct node* newnode=createnode(data);
-        
-        if(head==NULL){
-            head=newnode;
-            temp=head;
-        }
-        else{
-            temp->next=newnode;
-            temp=newnode;
+        printf("1.Insert at end\n2.Insert at beginning\n");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                insertend(&head,data,&temp);
+                break;
+            case 2:
+                insertbegin(&head,data);
+                break;
         }
        
-    }
-    
+       
+    }//for loop end
 
- 
-
-    printf("Before Insertion: \n");
+    printf("After Insertion: \n");
     traversal(head);
    
+    break;
+        case 2:
+        printf("\nvisit : https://www.pornhub.org/categories\n\n");
+        break;
+    }
    
    
     return 0;
